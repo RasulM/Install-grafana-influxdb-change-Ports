7cfe14db8e5436316970d12dc921d7f2dadd3a0d
Rasul Mahones <44272827+RasulM@users.noreply.github.com>
2019-10-30 22:09:08 -0400

    Update setports.py

--- setports.py
+++ setports.py
@@ -2,10 +2,10 @@ import os
 import configparser #/etc/grafana/grafana.ini
 
 config = configparser.ConfigParser()
-
+config.read("/etc/grafana/grafana.ini")
 
 PORT = input("What port you want grafana to listen on ")
-config.read("/etc/grafana/grafana.ini")
+#comment out config stuff if you get error
 config.set('server','http_port',PORT)
 # os.environ["GF_SERVER_HTTP_PORT"] = str(PORT)
 PORT = input("What port you want InfluxDB to listen on ")
